2af364805ab0740828007490af729d7979240473
Blues Young <15171255945@163.com>
2022-06-23 16:06:55 +0800

    feat: :sparkles: slide demo

---
+++ playground/slide-verify-demo/src/App.vue
@@ -0,0 +1,31 @@
+<!--
+ * @Author: zhangyang
+ * @Date: 2022-06-23 15:42:16
+ * @LastEditTime: 2022-06-23 15:47:13
+ * @Description: 
+-->
+<script setup lang="ts">
+import { YoungImgSlider } from '@bluesyoung/slide-verify';
+
+const slider = new YoungImgSlider(() => {
+  console.log('验证通过！')
+});
+
+</script>
+
+<template>
+  <div>
+    <button @click="slider.show()">开始</button>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
