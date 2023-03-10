38a8eb7854f0e71f54b3f434b4ba5c2cc1acada0
张扬 <bluesyoung_web@163.com>
2023-03-10 17:36:05 +0800

    test: :white_check_mark: add test case

---
+++ packages/ui-vue3-element-plus/src/test/useVerifyCode.spec.ts
@@ -0,0 +1,43 @@
+/*
+ * @Author: zhangyang
+ * @Date: 2023-03-10 16:55:47
+ * @LastEditTime: 2023-03-10 17:33:55
+ * @Description:
+ */
+import { describe, it, expect, vi, beforeAll, afterAll } from 'vitest';
+import { useVerifyCode } from '..';
+
+beforeAll(() => {
+  vi.useFakeTimers();
+});
+
+afterAll(() => {
+  vi.useRealTimers();
+});
+
+describe('验证码倒计时', () => {
+  const fn = vi.fn();
+  const { getCode, tip, showSlider, pass, cancel } = useVerifyCode(fn);
+  it('点击获取验证码', () => {
+    expect(tip.value).toMatchInlineSnapshot('"获取验证码"');
+    getCode();
+    expect(showSlider.value).toEqual(true);
+  });
+
+  it('人机验证通过, 开始倒计时, 计时结束后可以重新获取', async () => {
+    await pass();
+    expect(fn).toBeCalled();
+    expect(tip.value).toMatchInlineSnapshot('"59 秒后重试"');
+    await vi.advanceTimersByTimeAsync(1e3);
+    expect(tip.value).toMatchInlineSnapshot('"58 秒后重试"');
+    await vi.advanceTimersByTimeAsync(58e3);
+    expect(tip.value).toMatchInlineSnapshot('"0 秒后重试"');
+    await vi.advanceTimersByTimeAsync(1e3);
+    expect(tip.value).toMatchInlineSnapshot('"获取验证码"');
+  });
+
+  it('主动取消验证', () => {
+    cancel();
+    expect(showSlider.value).toEqual(false);
+  });
+});
