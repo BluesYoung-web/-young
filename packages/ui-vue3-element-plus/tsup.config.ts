a624cff1140ffba9cc289326e6854ed3aa51b4ec
张扬 <bluesyoung_web@163.com>
2023-07-26 09:51:17 +0800

    fix: :bug: fix tsup build error

--- packages/ui-vue3-element-plus/tsup.config.ts
+++ packages/ui-vue3-element-plus/tsup.config.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: zhangyang
  * @Date: 2022-04-28 14:22:55
- * @LastEditTime: 2023-03-25 19:47:53
+ * @LastEditTime: 2023-07-26 09:44:09
  * @Description: 仅作为 dts 生成器
  */
 import { defineConfig } from 'tsup';
@@ -10,5 +10,6 @@ export default defineConfig((options) => {
   return {
     entry: ['src/index.ts', 'src/resolver.ts'],
     dts: true,
+    external: ['@vue/shared', '@vue/reactivity', 'vue'],
   };
 });
